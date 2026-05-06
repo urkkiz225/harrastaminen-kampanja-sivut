2dd26a89fba5b7e6f7eb9ef51ce59f87a90f583a
urkkiz225 <urkkiz225@gmail.com>
2026-05-06 21:56:02 +0300

    aloitettu pop-up viikon työstäminen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/sections/Hero";
+import PopupPage from "../components/sections/PopupPage";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <PopupPage />
     </main>
   );
 }
