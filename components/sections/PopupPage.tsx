47153fb09f486fdaa6b287674d276b2fc75d7fc1
heiskas12 <sara.heiskanen@aalto.fi>
2026-05-11 20:46:32 +0300

    Muutos asetteluun

--- components/sections/PopupPage.tsx
+++ components/sections/PopupPage.tsx
@@ -5,7 +5,7 @@ export default function PopupPage() {
   return (
     <section id="aikataulu" className="bg-bg py-16 px-6 overflow-x-clip">
     <div className = "flex justify-center items-center text-center ">
-        <span className="block m-[13vw] mt-[2vw] scale-[1.2]"> {/*margin+scale combo vähä cursed mut tajusin liian myöhään et tein kaiken ihan liian pieneks hihhihii*/}
+        <span className="block m-[13vw] mt-[4vw] scale-[1.2]"> {/*margin+scale combo vähä cursed mut tajusin liian myöhään et tein kaiken ihan liian pieneks hihhihii*/}
             <DaySchedule/>
             <PopupWeekInfo/>
         </span>
